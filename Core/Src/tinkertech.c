d44322a1ee011fc9683a1ea8728f1ba7559d428f
lkghh <yoohyun2003@gmail.com>
2025-12-04 16:30:35 +0000

    Delete Debug part

--- Core/Src/tinkertech.c
+++ Core/Src/tinkertech.c
@@ -187,14 +187,6 @@ void loop() {
 		    double L = impedance/(2.0 * M_PI * freq);
 		    double C = 1.0/(2.0 * M_PI * freq * impedance);
 
-		    // checking phase diff and Magnitude
-			sprintf(msg, "DEBUG | Phase: %.4f | Mag: %.0f\r\n", phase_diff, mag_Vout);
-			HAL_UART_Transmit(&huart2, (uint8_t*)msg, strlen(msg), 100);
-			HAL_Delay(500);
-
-			// Calibrate phase difference
-			phase_diff += 5.05;
-
 
 		    // keeping phase difference in range (-PI - +PI)
 			if (phase_diff > M_PI) {
@@ -202,7 +194,6 @@ void loop() {
 			} else if (phase_diff < -M_PI) {
 			  phase_diff += (2.0 * M_PI);
 			}
-
 
 
 			// Print measurement
